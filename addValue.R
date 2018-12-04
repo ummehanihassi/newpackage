75827d118bb36b203ae001e223457dd6acfbf5e6
Ummehani <44399447+ummehanihassi@users.noreply.github.com>
2018-12-04 12:18:35 +0100

    Add files via upload

---
+++ addValue.R
@@ -0,0 +1,17 @@
+#' @title Add values
+#' @description A function that add values
+#' @param x one value
+#' @param y another value
+#' @details
+#'
+#' @return numeric value
+#'
+#' @example val <- addvalue(4,3)
+#' @export
+
+
+addvalue <- function(x,y){
+  return(x+y)
+}
+
+addvalue(4,3)
